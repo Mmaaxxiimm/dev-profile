6fe0664550d438d01c8d2ee181ed6d214d3ad65d
Maxim Thone <maximthone@MBPvanMximThone.lan>
2017-09-04 14:51:10 +0200

    eerste commit voor dit project

---
+++ dev-profile/dev-profile/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dev-profile
+//
+//  Created by Maxim Thone on 4/09/17.
+//  Copyright © 2017 Maxim. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
